8778432b5f1896c40bce2b60e201857b110118e2
Saim-Ahmed1 <78466954+Saim-Ahmed1@users.noreply.github.com>
2025-06-28 21:05:33 +0600

    first commit

--- app/(pages)/blogs/page.js
+++ app/(pages)/blogs/page.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { getAllBlogs } from '@/lib/blogs'
+import { getAllBlogs } from '../../../lib/blogs'
 
 export const metadata = {
   title: "iBlog - Blogs",
