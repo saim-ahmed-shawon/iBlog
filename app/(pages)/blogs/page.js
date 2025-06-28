5372fd35adbb0ff0fc1084cdab35d5527cb55dbe
Saim-Ahmed1 <78466954+Saim-Ahmed1@users.noreply.github.com>
2025-06-28 20:38:23 +0600

    first commit

--- app/(pages)/blogs/page.js
+++ app/(pages)/blogs/page.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 const Blogs = async () => {
  
-  const d = await fetch(`${process.env.SITE_URI}/api/blogs`)
+  const d = await fetch(`https://i-blog-five.vercel.app/api/blogs`)
   const posts = await d.json()
 
   return (
